043f66f0ae7d85c091b76d745490ccffc6ef3b81
Tomasz Budny <tombud93@gmail.com>
2017-03-26 18:54:29 +0200

    working char only

--- Cezar.java
+++ Cezar.java
@@ -2,88 +2,79 @@ package cezar;
 
 import java.util.Scanner;
 
-public class Cezar
-{
-	private String text;
-	private String cipher;
-	private int number;
+public class Cezar {
+	
+	public static void main(String[] args) {
+		System.out.println("O ile ma nastapic number Cezarze:");
 
-	public Cezar(String text, int number)
-	{
-	text=this.text;
-	number=this.number;
-	cipher="";
-	}
-	
-	
-    public static void main(String[] args)
-    {
-    System.out.println("O ile ma nastapic number Cezarze:");
-    
-    int number = 0;
-	
-	try {
-		Scanner scanner = new Scanner(System.in);
-		number = scanner.nextInt();
+		int number = 0;
+
+		try {
+			Scanner scanner = new Scanner(System.in);
+			number = scanner.nextInt();
 
 		}
 
-	catch(Exception e)
-	{
-	System.err.println("Blad "+e);
+		catch (Exception e) {
+			System.err.println("Blad " + e);
+		}
+
+		if (number != 0) {
+
+			System.out.println("Wprowadz tekst do zamiany:");
+
+			Scanner scanner = new Scanner(System.in);
+			String str = scanner.next();
+			
+			str = Encode(str,number);
+			System.out.println(str);
+			
+			str = Decode(str,number);
+			System.out.println(str);
+
+		
+			}
+
+		
+
 	}
 	
-	if(number!=0)
-		{
-		System.out.println("Wprowadz tekst do zamiany:");
-		
-		
-		Scanner scanner = new Scanner(System.in);
-		String str = scanner.next();
-		
-		Cezar cezar = new Cezar(str,number);
-		
-		System.out.println("Tekst:"+str);
-
-		
-    }
-	
-	public void szyfr (String text, int number)
+	private static String Encode(String str, int number)
 	{
+	char[] text = str.toUpperCase().toCharArray();
+		
+	for (int i = 0; i < str.length(); i++) {
+		if ((text[i] + number <= 90) && (text[i] + number >= 65)) {
+		
+			text[i]=(char)(text[i]+number);
+		
+		}
 
-
-	    for(int i=0;i<text.length();i++)
-	    {
-	        if(         (text.charAt(i)+number<=90) &&  (text.charAt(i)+number>=65) )
-	        {
-	        	text.charAt(i)=text.charAt(i)+number;
-	        }
-
-	        else if(    (text.charAt(i)-26+number>=65)  &&  (text.charAt(i)-26+number<=90)  )
-	        {
-	            text[i]=text.charAt(i)-26+number;
-	        }
-	    };
+		else if ((text[i] - 26 + number >= 65) && (text[i] - 26 + number <= 90)) {
+		
+			text[i]=(char)(text[i]+number-26);
+		
+		}
 	}
-
-
-	public void deszyfr (String text, int number)
-	{
-
-	    for(int i=0;i<text.length();i++)
-	    {
-	        if(        ((text.charAt(i)-number+26)>=65)    &&  (text.charAt(i)-number+26<90)       )
-	        {
-	            text[i]=text.charAt(i)+26-number;
-	        }
-
-	        else if(    (text.charAt(i)-number>=65)     &&  (text.charAt(i)-number<=90)    )
-	        {
-	            text[i]=text.charAt(i)-number;
-	        }
-	    };
+	return new String(text);
 	}
 	
+	private static String Decode(String str, int number)
+	{
+	char[] text =str.toUpperCase().toCharArray();
+	
+	for (int i = 0; i < str.length(); i++) {
+		if (((text[i] - number + 26) >= 65) && (text[i] - number + 26 < 90)) {
+			
+			text[i]=(char)(text[i]-number+26);
 
-    }
+		}
+
+		else if ((text[i] - number >= 65) && (text[i] - number <= 90)) {
+
+			text[i]=(char)(text[i]-number);
+		}
+	}
+	return new String(text);
+	}
 }
